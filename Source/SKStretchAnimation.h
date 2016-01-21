195b524db6d5bde968e0777e42d80c4f98becfcc
talisk <talisk@foxmail.com>
2016-01-21 21:53:23 +0800

    Initial commit.

---
+++ Source/SKStretchAnimation.h
@@ -0,0 +1,13 @@
+//
+//  SKStretchAnimation.h
+//  SKStretchTransition
+//
+//  Created by 孙恺 on 16/1/21.
+//  Copyright © 2016年 sunkai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SKStretchAnimation : NSObject
+
+@end
---
+++ Source/SKStretchAnimation.m
@@ -0,0 +1,13 @@
+//
+//  SKStretchAnimation.m
+//  SKStretchTransition
+//
+//  Created by 孙恺 on 16/1/21.
+//  Copyright © 2016年 sunkai. All rights reserved.
+//
+
+#import "SKStretchAnimation.h"
+
+@implementation SKStretchAnimation
+
+@end
