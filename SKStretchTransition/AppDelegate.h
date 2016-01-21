f0384c5fa211c963bc46d6eb304fe8e51c75279d
talisk <talisk@foxmail.com>
2016-01-21 21:29:57 +0800

    Initial Commit

---
+++ SKStretchTransition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SKStretchTransition
+//
+//  Created by 孙恺 on 16/1/21.
+//  Copyright © 2016年 sunkai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SKStretchTransition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SKStretchTransition
+//
+//  Created by 孙恺 on 16/1/21.
+//  Copyright © 2016年 sunkai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
